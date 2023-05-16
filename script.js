edab0063b71a8bbec560612da5cc3bf6f3a37b21
namakuandrew <thesky11.lay@gmail.com>
2023-05-16 16:33:59 +0700

    tambahan javascript dan perbaikan html

---
+++ script.js
@@ -0,0 +1,26 @@
+const scriptURL =
+"https://script.google.com/macros/s/AKfycbwu5kFUhm_qKGUGp_57JcQ-nXqMCSmoPVujq2BJV_FaF-R4RZYgxoM-psfo31yJBMOZrQ/exec"
+const form = document.forms["Andrew-contact-me"]
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset formnya
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
